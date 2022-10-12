f34b94eaeece27e0693e7389fcd694c58e931799
MarcosEspinoza-9 <70918015+MarcosEspinoza-9@users.noreply.github.com>
2022-10-12 15:01:44 -0500

    se asignaron estilos mediante un objeto externo de JavaScript

--- src/App.js
+++ src/App.js
@@ -4,17 +4,12 @@ import Carro from './Carro.js';
 import Moto from './Moto.js';
 
 export default function App() {
+//buscar cards en css para dar estilos a mi pagina
+
   return (
     <div>
       <h1>Hello React</h1>
-      <Carro Marca="Corvette C6"
-            Descripcion = "Vehiculo de alta velocidad"
-      />
-      <Moto
-        Marcas = "Yamaha R6"
-        descripcion = "Soy el mas veloz"
-      
-      />
+      <Carro Marca="Corvette C6" Descripcion="Vehiculo de alta velocidad" />
     </div>
   );
 }
--- src/Carro.js
+++ src/Carro.js
@@ -2,8 +2,17 @@ import React from 'react';
 import './style.css';
 
 export default function Carro(props) {
+    const estiloDiv ={
+      boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)"
+
+    };
+    const estilImgCard = {
+      
+
+    }
+  
   return (
-    <div class= "contenedor">
+    <div class= {{estiloDiv}}>
       <h2>Soy un carro {props.Marca} </h2>
       <img
         src="https://i.ytimg.com/vi/GUQ3DCM3GCQ/mqdefault.jpg"
